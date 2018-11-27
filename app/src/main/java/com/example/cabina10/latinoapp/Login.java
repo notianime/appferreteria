711d18c42c03615c82c9bb95a3a2af9c27a95d64
unknown <i1620840@continental.edu.pe>
2018-11-27 12:31:50 -0500

    Proyecto - Login - Validaciones

--- app/src/main/java/com/example/cabina10/latinoapp/Login.java
+++ app/src/main/java/com/example/cabina10/latinoapp/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,22 +18,38 @@ public class Login extends AppCompatActivity {
 
         etn = (EditText)findViewById(R.id.txt_Nombre);
         etp = (EditText)findViewById(R.id.txt_Contrasenia);
+
+        //AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "BD", null, 1);
+        //SQLiteDatabase BD  = admin.getWritableDatabase();
     }
-    //Metodo para el boton Ingresar
-    public void Registrar(View view){
+    //METODO PARA INICIAR SESION
+    public void Ingresar(View view){
         String nombre = etn.getText().toString();
         String contrasenia = etp.getText().toString();
 
         if (nombre.length() == 0){
             Toast.makeText(this, "Ingrese un nombre de Usuario", Toast.LENGTH_SHORT).show();
-        }
+            etn.requestFocus();
+            InputMethodManager inm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+            inm.showSoftInput(etn, InputMethodManager.SHOW_IMPLICIT);
+        }else
         if (contrasenia.length() == 0){
             Toast.makeText(this, "Ingrese una Contraseña", Toast.LENGTH_SHORT).show();
-        }
-        if (nombre.length() != 0 && contrasenia.length() != 0){
-            Toast.makeText(this, "Felicidades...!", Toast.LENGTH_SHORT).show();
+            etp.requestFocus();
+            InputMethodManager inm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+            inm.showSoftInput(etp, InputMethodManager.SHOW_IMPLICIT);
+        }else
+        if (!nombre.equals("")){
+            Toast.makeText(this, "Hola " + nombre, Toast.LENGTH_SHORT).show();
             Intent siguiente = new Intent(this, StudentsList.class);
+            siguiente.putExtra("jugador", nombre);
             startActivity(siguiente);
+            finish();
         }
+    }
+
+    @Override
+    public void onBackPressed(){
+
     }
 }
