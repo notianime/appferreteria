bf11c83f50f5551b8a86b9cc8601d9ffa39c7b92
notianime <i1028417@continental.edu.pe>
2018-12-16 22:43:27 -0500

    creacion de usuarios

--- app/src/main/java/com/example/progamador/myappferreteria/models/SubCategoria.java
+++ app/src/main/java/com/example/progamador/myappferreteria/models/SubCategoria.java
@@ -1,4 +1,34 @@
 package com.example.progamador.myappferreteria.models;
 
+import java.util.ArrayList;
+
 public class SubCategoria {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static ArrayList<String> getProductsAsString() {
+        ArrayList<String> subcat = new ArrayList<>();
+        subcat.add("Clavos de albañil");
+        subcat.add("Clavos uso general");
+        subcat.add("Fijaciones especiales");
+        subcat.add("Fijaciones para Drywall");
+        subcat.add("Kit de fijaciones");
+        return subcat;
+    }
 }
